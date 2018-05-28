f78ea3d6bea5b15f16c0b13ebecd949786472172
Stewart Boyd <stewartboyd1988@gmail.com>
2018-05-28 14:02:23 -0400

    Refactored app.js to use geocode.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const request = require('request');
-const querystring = require('querystring');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
-const address = 'address';
+const address= "address";
 const argv = yargs.
     option(address, {
         alias: 'a',
@@ -14,32 +13,12 @@ const argv = yargs.
     .alias('help', 'h')
     .argv;
 
+geocode.geocodeAddress(argv.address, (errorMessage, result) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else if (result) {
 
+        console.log(JSON.stringify(result, undefined, 2));
+    }
 
-var url_base = 'https://maps.googleapis.com/maps/api/geocode/json';
-var key = 'AIzaSyAY6ErpHabi_J_S5mdpVdKAElxOptI086A';
-var url_query = querystring.stringify(
-    {address: argv.address,
-    key}
-);
-var url = url_base + '?' + url_query;
-console.log(url);
-var request_object ={url: url, json: true} ;
-request(request_object,
-    (error, response, body) => {
-        if (error) {
-            console.log('Unable to connect to google servers');
-        } else if (response.statusCode == 404){
-            console.log(`${response.statusCode} is no good`);
-        } else if (body.status === 'ZERO_RESULTS') {
-            console.log('Unable to find that address');
-        } else if (body.status === 'OK') {
-
-            var result = body.results[0];
-            console.log(`Address = ${result.formatted_address}`)
-            console.log(`Latitue = ${result.geometry.location.lat}`);
-            console.log(`Latitue = ${result.geometry.location.lng}`);
-        } else {
-            console.log('Undefined error');
-        }
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,45 @@
+
+const request = require('request');
+const querystring = require('querystring');
+
+
+var url_base = 'https://maps.googleapis.com/maps/api/geocode/json';
+var api_key = 'AIzaSyAY6ErpHabi_J_S5mdpVdKAElxOptI086A';
+var geocodeAddress = (address, callback) => {
+    var url_query = querystring.stringify(
+        {address,
+        key: api_key}
+    );
+    var url = `${url_base}?${url_query}`;
+
+    var request_object ={url: url, json: true} ;
+
+    request(request_object,
+        (error, response, body) => {
+            var errorMessage = null;
+            var result = null;
+            if (error) {
+                errorMessage  = ('Unable to connect to google servers');
+            } else if (response.statusCode == 404){
+                errorMessage = (`${response.statusCode} is no good`);
+            } else if (body.status === 'ZERO_RESULTS') {
+                errorMessage = ('Unable to find that address');
+            } else if (body.status === 'OK') {
+
+                var _result = body.results[0];
+                 result = {
+                     address: _result.formatted_address,
+                     lat: _result.geometry.location.lat,
+                     lng: _result.geometry.location.lng
+
+                 }
+            } else {
+                errorMessage = ('Undefined error');
+            }
+            callback(errorMessage, result);
+    });
+}
+
+module.exports = {
+    geocodeAddress
+}
